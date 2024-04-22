476e66ea3675c48f63c1d7e35f2f223cfdcf859e
LukaPerisic <luka.perisic01@gmail.com>
2024-04-22 19:23:09 +0200

    Calendarov Header

--- client/src/components/CalendarHeader.js
+++ client/src/components/CalendarHeader.js
@@ -1,7 +1,26 @@
 import React from 'react';
+import calendar from '../assets/googlecalendar.png';
 
 const CalendarHeader = () => {
-  return <div>CalHeader</div>;
+  return (
+    <header className='px-4 py-2 flex items-center'>
+      <img src={calendar} alt='logo' className='mr-2 w-12 h-12' />
+      <h1 className='mr-10 text-xl text-gray-700 font-bold'>
+        Interactive Calendar
+      </h1>
+      <button className='border rounded text-gray-600 py-2 px-4 mr-5'>
+        Today
+      </button>
+      <button>
+        <span className='material-icons-outlined cursor-pointer text-gray-600 mx-2'>
+          chevron_left
+        </span>
+        <span className='material-icons-outlined cursor-pointer text-gray-600 mx-2'>
+          chevron_right
+        </span>
+      </button>
+    </header>
+  );
 };
 
 export default CalendarHeader;
