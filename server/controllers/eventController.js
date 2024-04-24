62179ea5b737f4a7a71b467d96a7b3a5e2bb3bc9
LukaPerisic <luka.perisic01@gmail.com>
2024-04-24 13:02:51 +0200

    ne comitovan eventController

--- server/controllers/eventController.js
+++ server/controllers/eventController.js
@@ -58,3 +58,67 @@ export const getEvents = async (req, res) => {
     });
   }
 };
+
+
+// GET /api/events/:id
+export const getEvent = async (req, res) => {
+  const { id } = req.params;
+
+  try {
+    Event.findOne({ _id: id }, (err, event) => {
+      if (err) {
+        return res.status(500).send({
+          error: 'Something went wrong while fetching the event: ' + err,
+        });
+      }
+
+      if (!event) {
+        return res.status(404).send({
+          error: 'No event found',
+        });
+      }
+
+      return res.status(200).send(event);
+    });
+  } catch (error) {
+    return res.status(500).send({
+      error: 'Something went wrong while fetching the event: ' + error,
+    });
+  }
+};
+
+// PUT /api/events/:id
+export const updateEvent = async (req, res) => {
+  try {
+    const { id } = req.params;
+
+    const body = req.body;
+    Event.updateOne({ _id: id }, body, (err, data) => {
+      if (err) throw err;
+
+      return res.status(202).send({
+        message: 'Event information updated',
+        event: data,
+      });
+    });
+  } catch (error) {
+    return res.status(500).send({
+      error: 'Something went wrong while updating updateEvent: ' + error,
+    });
+  }
+};
+
+// DELETE /api/events/:id
+export const deleteEvent = async (req, res) => {
+  try {
+    await Event.deleteMany({ _id: req.params.id });
+
+    return res.status(204).send({
+      message: 'Event deleted',
+    });
+  } catch (error) {
+    return res.status(500).send({
+      error: 'Something went wrong while deleteEvent: ' + error,
+    });
+  }
+};
